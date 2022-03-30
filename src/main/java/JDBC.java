6a4b7d04c0152c6a35010ad3b6151b888223c637
hezhongsheng <2558686381@qq.com>
2022-03-30 18:34:50 +0800

    首次提交

---
+++ src/main/java/JDBC.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class JDBC {
+    public static void main(String[] args) {
+        String user="root";
+        String password="123456";
+        String url = "jdbc:mysql://127.0.0.1:3306/world?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String sql="SELECT * FROM city";
+        Connection conn=null;
+        try {
+            conn= DriverManager.getConnection(url,user,password);
+            Statement statement = conn.createStatement();
+            ResultSet rs=statement.executeQuery(sql);
+            while (rs.next()){
+                System.out.println(rs.getString("Name"));
+            }
+            //System.out.println(conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
